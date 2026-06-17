83dad4d323cb24ee2140e448899e4787693e6b90
Rian Lucas <rlucasgit@gmail.com>
2026-06-17 14:33:49 -0300

    acesso ao banco de dados

--- db.py
+++ db.py
@@ -1,7 +1,28 @@
+
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+import pandas as pd
 
-load_dotenv()
 
-engine = create_engine(os.environ["DATABASE_URL"])
+class Database:
+
+    
+    def __init__(self):
+        load_dotenv()
+        self.load()
+
+        
+    def load(self):
+        self.engine = create_engine(os.environ["DATABASE_URL"])
+
+    
+    def query(self, query):
+        return pd.read_sql(query, self.engine)
+
+        
+    def select_all(self, limit=5):
+        query = f'SELECT * FROM "Leads" LIMIT {limit}'
+        return self.query(query)
+        
+
